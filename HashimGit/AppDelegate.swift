10279e8eae3e03ff6980ab0c9604770e42b4a206
Hashim M H <hashim.mh@philips.com>
2021-02-20 13:21:33 +0530

    first commit

---
+++ HashimGit/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  HashimGit
+//
+//  Created by Hashim M H on 20/02/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ HashimGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HashimGit
+//
+//  Created by Hashim M H on 20/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
