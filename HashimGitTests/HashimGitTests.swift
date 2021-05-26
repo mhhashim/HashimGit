b383ac071c6c8d0e398a6662d350631c7e320d51
Hashim M H <hashim.mh@philips.com>
2021-05-26 21:15:58 +0530

    testing github codecov

--- HashimGitTests/HashimGitTests.swift
+++ HashimGitTests/HashimGitTests.swift
@@ -19,15 +19,9 @@ class HashimGitTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let sut = TestClass()
+        let result = sut.testFunc(a:1, b:2)
+        XCTAssertEqual(result, 3)
     }
 
 }
